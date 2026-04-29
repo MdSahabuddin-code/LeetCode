87ae11cb8f833dd2bcb8562754c2e46013afbed3
Md Sahabuddin <91362098+MdSahabuddin-code@users.noreply.github.com>
2026-04-29 12:34:59 +0530

    Time: 17 ms (78.04%) | Memory: 45.4 MB (81.25%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -3,39 +3,25 @@ class Solution {
         int n=nums.length;
         Arrays.sort(nums);
         int max_diff=Integer.MAX_VALUE;
-        int left, right, sum, res_sum=0;
-        for (int i=0; i<n-2;i++){
-            left=i+1;
-            right=n-1;
-            while (left<right){
-                sum=nums[i]+nums[left]+nums[right];
-                if (sum == target){
-                    int diff=Math.abs(sum-target);
-                    if(max_diff>diff){
-                        max_diff=diff;
-                        res_sum=sum;
-                    }
-                    left++;
-                    right--;
+        int left, right, sum;
+        int res_sum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            left = i + 1; right = n - 1;
+
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(target - sum) < Math.abs(target - res_sum)) {
+                    res_sum = sum;
                 }
-                else if (sum < target){
-                    int diff=Math.abs(sum-target);
-                    if(max_diff>diff){
-                        max_diff=diff;
-                        res_sum=sum;
-                    }
-                    left++;
-                }
-                else if (sum > target){
-                    int diff=Math.abs(sum-target);
-                    if(max_diff>diff){
-                        max_diff=diff;
-                        res_sum=sum;
-                    }
-                    right--;
-                }
+
+                if (sum == target) return sum;
+
+                if (sum < target) left++;
+                else right--;
             }
         }
-        return res_sum;
-    }   
+        return res_sum;  
+    }
 }
